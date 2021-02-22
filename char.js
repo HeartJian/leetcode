028361ba29150867cd273f5776a209eb086b7f1a
fujianliang <fujianliang>
2021-02-22 10:31:29 +0800

    swapPairs

--- char.js
+++ char.js
@@ -44,3 +44,34 @@ var lengthOfLongestSubstring = function (s) {
     return result
 
 };
+
+
+// *  无重复字符的最长子串
+// *  给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+// *
+// *  输入: s = "abcabcbb"
+// *  输出: 3 
+// *  解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+var lengthOfLongestSubstring2 = (chars)=>{
+    const length = chars.length;
+    if(length == 1){
+        return 1
+    }
+    let max = 0,left=0,right=1,countMap={};
+    countMap[chars[left]] = 1;
+    while(left<length && right<length){
+        if(left == right){
+            right++
+        }else if( !countMap[chars[right]]){
+            countMap[chars[right]] = 1;
+            right ++ ;
+        }else{
+            delete countMap[chars[left]];
+            max = right-left > max? right-left:max;
+            left ++ ;
+            countMap[chars[left]]=1;
+        }
+    }
+    max = right-left> max? right-left:max;
+    return max;
+}
